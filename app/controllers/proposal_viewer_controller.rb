c17addc5356e73461456918fda34addac5851a1a
leszek <leszek@leszek-Lenovo-U310.(none)>
2012-12-07 10:24:28 +0100

    passing arguments to private controller methods it's a quiet bad idea

--- app/controllers/proposal_viewer_controller.rb
+++ app/controllers/proposal_viewer_controller.rb
@@ -1,13 +1,13 @@
 class ProposalViewerController < ApplicationController
   def show
-    client = Client.find(params[:id])
-    render :inline => resource_template.render(resource_presenter(client).as_template_data), :nothing => true
+    @client = Client.find(params[:id])
+    render :inline => resource_template.render(resource_presenter.as_template_data), :nothing => true
   end
 
   private
 
-  def resource_presenter(client)
-    @resource_presenter ||= ProposalViewerPresenter.new(client)
+  def resource_presenter
+    @resource_presenter ||= ProposalViewerPresenter.new(@client)
   end
 
   def resource_template
