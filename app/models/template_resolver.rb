fcd42bc98cf070b9a897a2faca5df6df59e89f34
leszek <leszek@leszek-Lenovo-U310.(none)>
2012-12-08 15:05:39 +0100

    template resolving comment

--- app/models/template_resolver.rb
+++ app/models/template_resolver.rb
@@ -1,6 +1,6 @@
 # place for managing templates
 # in the future, templates could be stored in other than File store
-# look at "Crafting Rails Applications.., Jose Vailm, Chapter 3"
+# look at "Crafting Rails Applications..,Jose Valim, Chapter 3"
 
 class TemplateResolver
   def initialize(request, template_file_path = "/public/proposal-template/index.html")
